eff6fc9be98684d0dcccb7897a1ea527c3c59216
조단비 <rarin2002@naver.com>
2021-08-24 16:51:55 +0900

    test

--- ObjectManager.cpp
+++ ObjectManager.cpp
@@ -5,8 +5,7 @@ ObjectManager* ObjectManager::Instance = nullptr;
 void ObjectManager::AddObject(Object* _Object,string _Key)
 {
 	list<Object*> Temp;
-	Temp.push_back(_Object);
-
+	Temp.push_back(new Object(_Object));
 	ObjectList[_Key] = Temp;
 }
 
@@ -14,4 +13,6 @@ void ObjectManager::Release()
 {
 	delete ObjectList;
 	ObjectList = nullptr;
+	delete Player;
+	Player = nullptr;
 }
